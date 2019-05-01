f8d7b609c8cc71c72d679a3f9d17be4e70ea8dff
Alexey Zhukov <hadzage@gmail.com>
2019-05-01 15:32:02 +0200

    version 1

---
+++ src/JavaSweeper.java
@@ -0,0 +1,68 @@
+import javax.swing.*;
+import java.awt.*;
+import sweeper.Box;
+import sweeper.Coord;
+import sweeper.Ranges;
+
+public class JavaSweeper extends JFrame {
+
+    private final int COLS = 9;
+    private final int ROWS = 9;
+
+    private final int IMAGE_SIZE = 50;
+    private JPanel panel;
+
+    public static void main(String[] args) {
+
+        new JavaSweeper();
+
+    }
+
+    private JavaSweeper () {
+        Ranges.setSize(COLS, ROWS);
+        setImages();
+        initPanel();
+        initFrame();
+    }
+    private void initPanel() {
+        panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                for (Coord coord : Ranges.getAllCoords()) {
+                    g.drawImage((Image)Box.values()[(2 * coord.x + coord.y) % Box.values().length].image,
+                            coord.x * IMAGE_SIZE,
+                            coord.y * IMAGE_SIZE, this);
+                }
+
+
+            }
+        };
+        panel.setPreferredSize(new Dimension(Ranges.getSize().x * IMAGE_SIZE,
+                                             Ranges.getSize().y * IMAGE_SIZE));
+        add (panel);
+    }
+
+    private void initFrame() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Java Sweeper");
+        setResizable(false);
+        pack();
+        setVisible(true);
+        setLocationRelativeTo(null);
+    }
+
+    private void setImages() {
+        for (Box box : Box.values()) {
+            box.image = getImage(box.name().toLowerCase());
+        }
+        setIconImage(getImage("icon"));
+    }
+
+    private Image getImage (String name) {
+        String filename = "/img/" + name + ".png";
+        ImageIcon icon = new ImageIcon(getClass().getResource(filename));
+        return icon.getImage();
+    }
+
+}
---
+++ src/sweeper/Ranges.java
@@ -0,0 +1,32 @@
+package sweeper;
+
+import com.sun.prism.image.Coords;
+
+import java.util.ArrayList;
+
+public class Ranges {
+
+    static private Coord size;
+    static private ArrayList<Coord> allCoords;
+
+    public static ArrayList<Coord> getAllCoords() {
+        return allCoords;
+    }
+
+    static void setSize (Coord size) {
+        Ranges.size = size;
+        allCoords = new ArrayList<Coord>();
+        for (int x = 0; x < size.x; x++)
+            for (int y = 0; y < size.y; y++)
+                allCoords.add(new Coord(x, y));
+    }
+
+    static public Coord getSize () {
+        return size;
+    }
+
+    static public void setSize (int cols, int rows) {
+        Coord size = new Coord (cols, rows);
+        setSize(size);
+    }
+}
